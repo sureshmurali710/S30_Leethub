645b3ef0c6453ca4f4d14ad86878ec35e38dca26
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2021-12-05 22:16:16 -0500

    Time: 3 ms (82.88%), Space: 46.8 MB (63.84%) - LeetHub

---
+++ find-the-town-judge/find-the-town-judge.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int [] indegrees = new int[n];
+        for(int [] t:trust){
+            indegrees[t[0] -1]--;
+            indegrees[t[1] -1]++;
+        }
+        
+        for(int i = 0; i < indegrees.length; i++){
+            if(indegrees[i] == n-1){
+                return i + 1;
+            }
+        }
+        
+        return -1;
+    }
+}
