48e68e42efef71d58a2d87334c890df23b4bc9b1
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-01-13 15:57:01 -0500

    Time: 0 ms (100.00%), Space: 40.4 MB (30.74%) - LeetHub

--- 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.java
+++ 154-find-minimum-in-rotated-sorted-array-ii/154-find-minimum-in-rotated-sorted-array-ii.java
@@ -1,29 +1,21 @@
-public class Solution {
-  
-  public int findMin(int[] a) {
-    return search(a, 0, a.length - 1);
-  }
-  
-  private int search(int[] a, int lo, int hi) {
-    // a[] is sorted
-    if (lo > hi)
-      return a[0];
+class Solution 
+{
+    public int findMin(int[] A) 
+    {
+        int start = 0;
+		int end = A.length - 1;
         
-    int mid = lo + (hi - lo) / 2;
-    
-    if (mid > 0 && a[mid - 1] > a[mid])
-      return a[mid];
-    
-    if (mid < a.length - 1 && a[mid] > a[mid + 1])
-      return a[mid + 1];
-        
-    if (a[lo] == a[mid] && a[mid] == a[hi])
-      return Math.min(search(a, lo, mid - 1), search(a, mid + 1, hi));
-        
-    if (a[lo] <= a[mid])
-      return search(a, mid + 1, hi);
-    else
-      return search(a, lo, mid - 1);
-  }
+        while(start < end)
+        {
+            int mid = start + (end - start) / 2;
 
+            if(A[mid] > A[end])					// Minimum must be in A[mid + 1, end]
+                start = mid + 1;				
+            else if(A[mid] < A[end])			// Minimum must be in A[start, mid]
+                end = mid;
+			else // if(A[mid] == A[end])		// Skip duplicates
+				end--;
+        }
+        return A[start];						// Loop ends when start == end
+    }
 }
