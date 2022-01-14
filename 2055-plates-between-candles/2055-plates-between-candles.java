90e93856f6da0804987fb7f4fa5b78185c6c38fb
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-01-14 12:15:52 -0500

    Time: 13 ms (63.90%), Space: 124.5 MB (25.05%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.java
+++ 2055-plates-between-candles/2055-plates-between-candles.java
@@ -1,16 +1,31 @@
 class Solution {
     public int[] platesBetweenCandles(String s, int[][] queries) {
-          TreeMap<Integer, Integer> m = new TreeMap<>();
-          for (int i = 0, count = 0; i < s.length(); i++)
-                if (s.charAt(i) == '|')
-                    m.put(i, count);
-                else count++;
-          int[] r = new int[queries.length];
-          for (int i = 0; i < queries.length; i++) {
-            Map.Entry<Integer, Integer> left = m.ceilingEntry(queries[i][0]), right = m.floorEntry(queries[i][1]);
-            if (left != null && right != null)
-                r[i] = Math.max(0, right.getValue() - left.getValue());
+        int n = s.length();
+        int m = queries.length;
+        int[] prefix = new int[n];
+        char[] arr = s.toCharArray();
+        int curCnt = 0;
+        int cnt = 0;
+        for(int i = 0; i < n; i++){
+            if(arr[i] == '*') curCnt++;
+            else cnt = curCnt;
+            prefix[i] = cnt;
+        }
+        int[] forward = new int[n];
+        int p = n;
+        for(int i = n - 1; i >= 0; i--){
+            if(arr[i] == '|') p = i;
+            forward[i] = p;
+        }
+        // System.out.println(Arrays.toString(prefix));
+        int[] ans = new int[m];
+        for(int i = 0; i < m; i++){
+            int[] query = queries[i];
+            if(forward[query[0]] < n && forward[query[0]] <= query[1]){
+                // System.out.println("right left: " + query[1] + " " + query[0]);
+                ans[i] = prefix[query[1]] - prefix[forward[query[0]]];
             }
-            return r;
+        }
+        return ans;
     }
 }
