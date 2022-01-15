5d3eb74b7fa6b700cff9a5aa9879a90fb898a112
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-01-14 19:18:31 -0500

    Time: 0 ms (100.00%), Space: 85 MB (52.30%) - LeetHub

--- 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
+++ 1901-find-a-peak-element-ii/1901-find-a-peak-element-ii.java
@@ -1,24 +1,55 @@
 class Solution {
     public int[] findPeakGrid(int[][] mat) {
-        int x=0, y=0, m = mat.length, n = mat[0].length ;
-        int [][]a = {
-            {-1,0},{1,0},{0,1},{0,-1}
-        }; 
-        while(true){
-            boolean flag = true;
-            for(int i=0;i<4;i++){
-                int x1 = x + a[i][0];
-                int y1 = y + a[i][1];
-                if(x1 >= 0 && y1 >= 0 && x1 < m && y1 < n && mat[x][y] < mat[x1][y1]){
-                    flag = false;
-                    x = x1;
-                    y = y1;
-                }
-            }
-            if(flag){
-                return new int[]{x,y};
-            }
+       int n = mat.length, m = mat[0].length;
+        
+        int lo = 0, hi = m-1;
+        
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            int[] temp = f1(mat, mid, n, m);
+            
+            if(temp[1] == mid) return temp;
+            else if(temp[1] == mid+1) lo = mid+1;
+            else hi = mid - 1;
         }
+        
+        return null;
+        
+    }
+    
+    // Find max of the three columns
+    public int[] f1(int[][] mat, int mid, int n, int m){
+        
+        int max = Integer.MIN_VALUE;
+        int[] ans = new int[2];
+        
+        for(int i=0;i<n;i++){
+            
+            // Left column
+            if(mid-1>=0 && mat[i][mid-1]>max){
+                max = mat[i][mid-1];
+                ans[0] = i;
+                ans[1] = mid-1;
+            }
+            
+            // Central column
+            if(mat[i][mid]>max){
+                max = mat[i][mid];
+                ans[0] = i;
+                ans[1] = mid;
+            }
+            
+            // Right column
+            if(mid+1<m && mat[i][mid+1]>max){
+                max = mat[i][mid+1];
+                ans[0] = i;
+                ans[1] = mid+1;
+            }
+
+        }
+        
+        return ans;
+        
     
     }
     }
