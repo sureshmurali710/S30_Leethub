79f79f6f6885a0635bac0465fa472e125e5da8e6
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-01-17 21:46:21 -0500

    Time: 1201 ms (5.05%), Space: 52.5 MB (15.48%) - LeetHub

--- 221-maximal-square/221-maximal-square.java
+++ 221-maximal-square/221-maximal-square.java
@@ -1,24 +1,71 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
-        if(matrix == null || matrix.length == 0) return 0;
-        int max = 0;
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        int dp[][] = new int[m+1][n+1];
-        
-        for(int i = 1; i< dp.length; i++){
-            for(int j = 1; j< dp[0].length; j++){
-                boolean flag = false;
-                if(matrix[i-1][j-1] == '1'){
-                   dp[i][j] = Math.min(dp[i][j-1], Math.min(dp[i-1][j], dp[i-1][j-1])) + 1;
-                    max = Math.max(max, dp[i][j]);
-                
-            } 
+        int rows = matrix.length;
+
+        if(rows == 0) return 0;
+
+        int curlen = 0;
+
+        int cols = matrix[0].length;
+
+        boolean flag = false;
+
+        int maxLen = 0;
+
+        for(int i = 0; i < rows; i++){
+
+            for(int j = 0; j < cols; j++){
+
+                if(matrix[i][j] == '1'){
+
+                    curlen = 1;
+
+                    flag = true;
+
+                    while(i + curlen < rows && j + curlen < cols && flag){
+
+                        //[i+curlen] [j+curlen]
+
+                        for(int k = i; k <= i+ curlen; k++){
+
+                            if(matrix[k][j+curlen] == '0'){
+
+                                flag = false;
+
+                                break;
+
+                            }
+
+                        }
+
+                        for(int k = j; k <= j+ curlen; k++){
+
+                            if(matrix[i+curlen][k] == '0'){
+
+                                flag = false;
+
+                                break;
+
+                            }
+
+                        }
+
+                        if(flag) curlen++;
+
+                    }
+
+                  maxLen = Math.max(maxLen, curlen);
+
+                }
+
             }
-                
+
         }
-        return max*max;
-        
+
+        return maxLen*maxLen;
+
     }
+
 }
+        
+    
