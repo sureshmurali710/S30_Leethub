5bc25665329b0b85a273d5a15f23c6f4ff4daa1d
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-01-20 13:14:16 -0500

    Time: 0 ms (100.00%), Space: 38.3 MB (28.63%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,17 +1,23 @@
 class Solution {
-  public int rob(int[] nums) {
-        if(nums.length==1)
-            return nums[0];
-        return Math.max(robb(nums,0,nums.length-1),robb(nums,1,nums.length));
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        
+        return Math.max(rob(nums, 0, nums.length-1), rob(nums, 1, nums.length));
+            
+
     }
-    public int robb(int[] nums,int start,int end)
-    {
-        int prev_house=0,prev_prev_house=0,max=0;
-        for(int i=start;i<end;i++)
-        {
-            max=Math.max(prev_house,prev_prev_house+nums[i]);
-            prev_prev_house=prev_house;
-            prev_house=max;
+    
+    private int rob(int[] nums, int start, int end){
+        int prev_max =0;
+        int prev = 0;
+        int max = 0;
+        
+        for(int i = start;i < end; i++){
+            max = Math.max(prev_max + nums[i], prev);
+            prev_max = prev;
+            prev = max;
+            
         }
         return max;
     }
