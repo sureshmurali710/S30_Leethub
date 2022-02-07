db876d46329103867ec39536b7d2c2177905e699
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-02-06 23:27:24 -0500

    Time: 1 ms (74.26%), Space: 48.5 MB (5.70%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,37 +1,47 @@
-class Solution {
-    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        
-     
-        if(image[sr][sc] == newColor || image.length == 0 || image[0].length == 0 ||
-          image == null) return image;
+class Solution
 
-        Queue<int []> q = new LinkedList<>();
-        int color = image[sr][sc];
-        image[sr][sc] = newColor;
-        
-        int [][] dirs = {{0,1},{1,0},{-1,0},{0,-1}};
-        q.add( new int[]{sr, sc});
-        
-        while(!q.isEmpty()){
-            int size = q.size();
-            int[] curr = q.poll();
-            for(int [] dir: dirs){
-                int r = dir[0] + curr[0];
-                int c = dir[1] + curr[1];
-                
-                if(r >= 0 && r < image.length &&  c < image[0].length && c >= 0 && image[r][c] == color){
-                
-                    image[r][c] = newColor;
-                    q.add(new int []{r, c});
-                }
-            }
-            
-            
-        }
-        
-        return image;
-        
-        
-        
-    }
-}
+{ 
+
+  public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
+
+{ if (image[sr][sc] == newColor)
+
+return image; 
+
+int color = image[sr][sc]; 
+
+dfs(image,sr,sc,newColor, color); 
+
+return image; 
+
+} 
+
+private void dfs(int[][] image, int sr, int sc, int newColor, int color)
+
+{ // base case 
+
+if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) 
+
+return; 
+
+//logic 
+
+image[sr][sc] = newColor; 
+
+int [][] dirs = new int[][] 
+
+{{0,1}, {1,0}, {-1,0}, {0,-1}}; 
+
+for(int[] dir : dirs)
+
+{ 
+
+int r = sr + dir[0]; 
+
+int c = sc + dir[1]; 
+
+dfs(image, r , c, newColor, color); 
+
+} 
+
+}}
