b5061ba4cd9c53354e0803359d1920181cc5fe3f
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-02-06 23:30:01 -0500

    Time: 2 ms (31.53%), Space: 47.9 MB (8.51%) - LeetHub

--- 733-flood-fill/733-flood-fill.java
+++ 733-flood-fill/733-flood-fill.java
@@ -1,47 +1,34 @@
-class Solution
-
-{ 
-
-  public int[][] floodFill(int[][] image, int sr, int sc, int newColor)
-
-{ if (image[sr][sc] == newColor)
-
-return image; 
-
-int color = image[sr][sc]; 
-
-dfs(image,sr,sc,newColor, color); 
-
-return image; 
-
-} 
-
-private void dfs(int[][] image, int sr, int sc, int newColor, int color)
-
-{ // base case 
-
-if(sr < 0 || sr >= image.length || sc < 0 || sc >= image[0].length || image[sr][sc] != color) 
-
-return; 
-
-//logic 
-
-image[sr][sc] = newColor; 
-
-int [][] dirs = new int[][] 
-
-{{0,1}, {1,0}, {-1,0}, {0,-1}}; 
-
-for(int[] dir : dirs)
-
-{ 
-
-int r = sr + dir[0]; 
-
-int c = sc + dir[1]; 
-
-dfs(image, r , c, newColor, color); 
-
-} 
-
-}}
+class Solution {
+    int [][] dirs;
+    int color;
+    int m;
+    int n;
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        
+     
+        if(image[sr][sc] == newColor || image.length == 0 || image[0].length == 0 ||
+          image == null) return image;
+        m = image.length;
+        n = image[0].length;
+        color = image[sr][sc];
+        dirs = new int [][]{{0,1},{1,0},{-1,0},{0,-1}};
+        dfs(image, sr, sc, newColor);
+        return image; 
+        
+    }
+    
+    private void dfs(int[][] image, int sr, int sc, int newColor){
+        //base
+        if(sr < 0 || sr >= m || sc >= n || sc < 0 || image[sr][sc] != color) return;
+        //logic
+        image[sr][sc] = newColor;
+        for(int [] dir: dirs){
+            int r = sr + dir[0];
+            int c = sc + dir[1];
+            dfs(image, r, c, newColor);
+        }
+    
+        
+        
+    }
+}
