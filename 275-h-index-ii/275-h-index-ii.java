03d4671fb40d66b78e4b910b33ce20cccddf4039
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-03-09 23:47:49 -0500

    Time: 1 ms (27.50%), Space: 52.6 MB (22.98%) - LeetHub

---
+++ 275-h-index-ii/275-h-index-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) return 0;
+        
+        
+        int low = 0;
+        int high = citations.length - 1;
+        int n = citations.length;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(citations[mid] == n- mid){
+                return n-mid;
+            }else if(citations[mid] < n - mid){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+            
+            
+        }
+        return n - low;
+    }
+}
+
+//ologn
