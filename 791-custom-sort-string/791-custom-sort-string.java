c17d00c075a8d67be46e03d19ef844a0d687bdd3
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-06-08 03:16:19 -0400

    Time: 1 ms (75.95%), Space: 40 MB (96.89%) - LeetHub

--- 791-custom-sort-string/791-custom-sort-string.java
+++ 791-custom-sort-string/791-custom-sort-string.java
@@ -1,37 +1,34 @@
 class Solution {
     public String customSortString(String order, String s) {
-        StringBuilder sb = new StringBuilder(); 
-        
         HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
         
-        for(int i = 0 ; i < s.length(); i++){
+        for(int i = 0;i < s.length(); i++){
             char c = s.charAt(i);
-            map.put(c, map.getOrDefault(c, 0)+1);
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
         for(int i = 0; i < order.length(); i++){
             char c = order.charAt(i);
-            
             if(map.containsKey(c)){
                 int count = map.get(c);
-            
                 while(count != 0){
                     sb.append(c);
                     count--;
                 }
-                map.remove(c);
             }
-            
+            map.remove(c);
         }
         
-        for(char c: map.keySet()){
+        for(char c : map.keySet()){
             int count = map.get(c);
             while(count != 0){
-                sb.append(c);
-                count--;
-            }
+                    sb.append(c);
+                    count--;
+                }
         }
         
         return sb.toString();
+        
     }
 }
