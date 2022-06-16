98c1df779a718db28e3a16c6afe29b09f1087f60
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-06-16 03:46:27 -0400

    Time: 100 ms (59.57%), Space: 148.4 MB (46.30%) - LeetHub

--- 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
+++ 2260-minimum-consecutive-cards-to-pick-up/2260-minimum-consecutive-cards-to-pick-up.java
@@ -1,14 +1,16 @@
 class Solution {
-    public int minimumCardPickup(int[] cards)
-    {
-        Map<Integer,Integer> map = new HashMap<>();
+    public int minimumCardPickup(int[] cards) {
+        HashMap<Integer, Integer> map  = new HashMap<>();
+        
         int min = Integer.MAX_VALUE;
-        for(int i = 0; i < cards.length; i++)
-        {
-            if(map.containsKey(cards[i]))
-                min = Math.min((i-map.get(cards[i]))+1,min); // Check if the difference in indices is smaller than minimum
-            map.put(cards[i],i); // Update the last found index of the card
+        
+        for(int i =0; i < cards.length; i++){
+            if(map.containsKey(cards[i])){
+                min = Math.min((i - map.get(cards[i])+1),min);
+            }
+            map.put(cards[i], i);
         }
-        return min == Integer.MAX_VALUE?-1:min; // Repetition found or not
+        return min == Integer.MAX_VALUE?-1:min;
+        
     }
 }
