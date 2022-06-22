ac0cf16fedbe1bdad79552ef88ee69d1c5195740
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-06-21 22:28:00 -0400

    Time: 234 ms (76.32%), Space: 41.9 MB (92.55%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.java
+++ 686-repeated-string-match/686-repeated-string-match.java
@@ -1,15 +1,13 @@
 class Solution {
     public int repeatedStringMatch(String a, String b) {
-        int alen = a.length();
-        int blen = b.length();
-		int mul = blen/alen;           //at least this much we need to a to itself
-        String tempa = a;
-        for(int i=1;i<(3+mul);i++){
-            if(a.contains(b)){
-                return i;
-            }
-            a += tempa;
-        }
+       StringBuilder res= new StringBuilder();
+        int count=0;
+        while(res.length()<b.length()){
+            res.append(a);
+            count++;
+        } 
+        if (res.toString().contains(b)) return count;
+        if(res.append(a).toString().contains(b)) return ++count;
         return -1;
     }
-    }
+}
