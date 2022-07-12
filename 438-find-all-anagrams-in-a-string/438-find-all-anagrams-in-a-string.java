f3498c833aa55c747f94827a3fcf2de7a85590ad
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-07-12 14:30:09 -0400

    Time: 32 ms (42.20%), Space: 48.2 MB (40.34%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,48 +1,39 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        
         List<Integer> result = new ArrayList<>();
-        
-        HashMap<Character,Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
         
         int match = 0;
         
-        
         if(s == null || s.length() == 0) return result;
         
-        for(int i =0; i<p.length(); i++){
-            char c  = p.charAt(i);
+        for(int i =0; i < p.length(); i++){
+            char c = p.charAt(i);
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
         
-        for(int i = 0; i < s.length(); i++){
+        for(int i = 0; i < s.length();i++){
             char in = s.charAt(i);
             
             if(map.containsKey(in)){
                 int count = map.get(in);
                 count--;
                 map.put(in, count);
-                
                 if(count == 0) match++;
             }
-            
-            if(i >= p.length()){
+            if( i>= p.length()){
                 char out = s.charAt(i - p.length());
                 if(map.containsKey(out)){
-                int count = map.get(out);
-                count++;
-                map.put(out, count);
-                
-                if(count == 1) match--;
+                    int count =map.get(out);
+                    count++;
+                    map.put(out, count);
+                    
+                    if(count == 1) match--;
+                }
             }
-            }
-            if(match == map.size()) result.add(i - p.length() + 1);
+            if(match == map.size()) result.add( i - p.length() + 1);
         }
         
-        
-        
         return result;
-        
-        
     }
 }
