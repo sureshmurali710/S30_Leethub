9284a4a1d4a252087d52cc010b3148c01395f04b
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-07-14 15:40:04 -0400

    Time: 15 ms (46.02%), Space: 55.5 MB (67.71%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,11 +1,12 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-         HashMap<String, List<String>> map = new HashMap<>();
+        HashMap<String, List<String>> map = new HashMap<>();
         
-        for(String s : strs){
-            char [] charArr = s.toCharArray();
-            Arrays.sort(charArr);
-            String sorted = String.valueOf(charArr);
+        for(String s: strs){
+            char[] c = s.toCharArray();
+            Arrays.sort(c);
+            
+            String sorted = String.valueOf(c);
             if(!map.containsKey(sorted)){
                 map.put(sorted,new ArrayList<>());
             }
