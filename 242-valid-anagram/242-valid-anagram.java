4cef36adbcdd3df9f6a6376f6e54d336ab2e1d93
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-07-14 12:41:41 -0400

    Time: 7 ms (53.66%), Space: 44.9 MB (39.42%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char [] c = s.toCharArray();
+        char [] d = t.toCharArray();
+        
+        Arrays.sort(c);
+        Arrays.sort(d);
+        
+        return String.valueOf(c).equals(String.valueOf(d));
+    }
+}
