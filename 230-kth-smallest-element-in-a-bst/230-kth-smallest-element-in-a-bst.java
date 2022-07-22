da12ed209fe23fbd55b75f07debb10a091535c9d
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2022-07-22 14:34:52 -0400

    Time: 1 ms (79.72%), Space: 41.9 MB (96.35%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -14,29 +14,20 @@
  * }
  */
 class Solution {
-    int result =0;
-    int count = 0;
     public int kthSmallest(TreeNode root, int k) {
-        inorder(root, k);
-        return result;
-    }
-    
-    private void inorder(TreeNode root, int k){
-        if(root == null){
-            return;
+        Stack<TreeNode> st = new Stack<>();
+        
+        while(root != null || !st.isEmpty()){
+            while(root != null){
+                st.push(root);
+                root = root.left;
+            }
+            root = st.pop();
+            k--;
+            if(k== 0) return root.val;
+            root = root.right;
         }
-        
-        inorder(root.left, k);
-        count++;
-        
-        if(count == k){
-            result = root.val;
-        }
-        
-        if(count < k){
-            inorder(root.right, k);
-        }
-        
+        return -1;
         
     }
 }
